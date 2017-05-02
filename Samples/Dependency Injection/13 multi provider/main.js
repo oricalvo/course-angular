8617489dfb6a25a699d44e2c0ea45a585b4a5ddb
ocalvo <ori.calvo@gmail.com>
2017-05-02 07:43:57 +0300

    More

--- Samples/Dependency Injection/13 multi provider/main.js
+++ Samples/Dependency Injection/13 multi provider/main.js
@@ -8,34 +8,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var MyService1 = (function () {
-    function MyService1(s) {
+var MyService = (function () {
+    function MyService() {
         console.log("MyService");
     }
-    return MyService1;
+    return MyService;
 }());
-MyService1 = __decorate([
+MyService = __decorate([
     core_1.Injectable(),
-    __param(0, core_1.Inject(core_1.forwardRef(function () { return MyService2; }))),
-    __metadata("design:paramtypes", [MyService2])
-], MyService1);
-var MyService2 = (function () {
-    function MyService2(s) {
-    }
-    return MyService2;
-}());
-MyService2 = __decorate([
-    core_1.Injectable(),
-    __metadata("design:paramtypes", [MyService1])
-], MyService2);
+    __metadata("design:paramtypes", [])
+], MyService);
 var injector = core_1.ReflectiveInjector.resolveAndCreate([
-    MyService1,
-    MyService2,
+    { provide: MyService, useClass: MyService, multi: true },
+    { provide: MyService, useClass: MyService, multi: true },
 ]);
-var obj = injector.get(MyService1);
+var obj = injector.get(MyService);
 console.log(obj);
--- Samples/Dependency Injection/13 multi provider/main.ts
+++ Samples/Dependency Injection/13 multi provider/main.ts
@@ -1,22 +1,16 @@
 import {Injectable, InjectionToken, ReflectiveInjector, Compiler, Optional, forwardRef, Inject} from "@angular/core";
 
 @Injectable()
-class MyService1 {
-    constructor(@Inject(forwardRef(()=>MyService2)) s: MyService2) {
+class MyService {
+    constructor() {
         console.log("MyService");
     }
 }
 
-@Injectable()
-class MyService2 {
-    constructor(s: MyService1) {
-    }
-}
-
 const injector = ReflectiveInjector.resolveAndCreate([
-    MyService1,
-    MyService2,
+    {provide: MyService, useClass: MyService, multi: true},
+    {provide: MyService, useClass: MyService, multi: true},
 ]);
 
-const obj = injector.get(MyService1);
+const obj = injector.get(MyService);
 console.log(obj);
